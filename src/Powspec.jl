65b45302dd78d55888cd535e3864208337998b20
Daniel Forero-Sanchez <df.forero10@gmail.com>
2023-09-06 01:27:05 +0200

    Make path dependent on environent var

--- src/Powspec.jl
+++ src/Powspec.jl
@@ -3,7 +3,7 @@ module Powspec
 using Parameters
 
 export power_spectrum
-
+println("Trying to locate library in ", ENV["LIBPOWSPEC_PATH"])
 include("structs.jl")
 include("box_periodic.jl")
 include("box_randoms.jl")
